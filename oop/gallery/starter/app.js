d94af04c157349065e8b8639ab5547464ef59313
tal0311 <tal.amit0311@gmail.com>
2021-12-27 22:28:45 +0200

    modal open

--- oop/gallery/starter/app.js
+++ oop/gallery/starter/app.js
@@ -8,67 +8,37 @@ function getElement(selection) {
   )
 }
 
-function Gallery(element) {
-  this.container = element
-  this.list = [...element.querySelectorAll('.img')]
-  // target
-  this.modal = getElement('.modal')
-  this.modalImg = getElement('.main-img')
-  this.imageName = getElement('.image-name')
-  this.modalImages = getElement('.modal-images')
+class Gallery {
+  constructor(element) {
+    this.container = element
+    this.list = [...element.querySelectorAll('.img')]
+    console.log('this.list:', this.list)
+    this.modal = getElement('.modal')
+    this.modalImg = getElement('.modal-img')
+    this.modalImgs = getElement('.modal-images')
+    this.closeBtn = getElement('.close-btn')
+    this.nextBtn = getElement('.next-btn')
+    this.prevBtn = getElement('.prev-btn')
 
-  this.closeBtn = getElement('.close-btn')
-  this.nextBtn = getElement('.next-btn')
-  this.prevBtn = getElement('.prev-btn')
+    // SELF REFERENCE POINTS TO GALLERY
+    // let self = this
+    // bind functions
 
-  // bind function
-
-  // container event
-  this.container.addEventListener(
-    'click',
-    function (e) {
-      if (e.target.classList.contains('img')) {
-        this.openModal(e.target, this.list)
-      }
-    }.bind(this)
-  )
+    this.container.addEventListener(
+      'click',
+      function (e) {
+        console.log(self)
+        this.openModel()
+      }.bind(this)
+    )
+  }
+  openModel() {
+    this.modal.classList.add()
+    console.log('open modal')
+  }
 }
 
-Gallery.prototype.openModal = function (selectedImage, list) {
-  this.setMainImage(selectedImage)
-  this.modalImages.innerHTML = list
-    .map(function (image) {
-      return ` <img
-        src="${image.src}"
-        title="${image.title}"
-        data-id="${image.dataset.id}"
-        class="${
-          selectedImage.dataset.id === image.dataset.id
-            ? 'modal-img selected'
-            : 'modal-img'
-        }"
-        alt="${image.alt}"
-      />`
-    })
-    .join('')
-  this.modal.classList.add('open')
-  this.closeBtn.addEventListener('click', this.closeModal.bind(this))
-  this.nextBtn.addEventListener('click', this.nextImage.bind(this))
-  this.prevBtn.addEventListener('click', this.prevImage.bind(this))
-}
+let nature = new Gallery(getElement('.nature'))
+let city = new Gallery(getElement('.city'))
 
-Gallery.prototype.setMainImage = function (selectedImage) {
-  this.modalImg.src = selectedImage.src
-  this.imageName = selectedImage.title
-}
-
-Gallery.prototype.closeModal = function () {
-  this.modal.classList.remove('open')
-  this.closeBtn.removeEventListener('click', this.closeModal.bind(this))
-  this.nextBtn.removeEventListener('click', this.nextImage.bind(this))
-  this.prevBtn.removeEventListener('click', this.prevImage.bind(this))
-}
-Gallery.prototype.nextImage = function () {}
-Gallery.prototype.prevImage = function () {}
-const nature = new Gallery(getElement('.nature'))
-const city = new Gallery(getElement('.city'))
+console.log(nature, city)
